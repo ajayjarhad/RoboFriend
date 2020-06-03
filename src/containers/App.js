1fd7848c29e4171c95d0351ed7724255e158ecab
Ajay Jarhad <ajayjarhad1@gmail.com>
2020-06-04 03:57:55 +0530

    Added Scroll.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
 import './App.css'
 
 class App extends React.Component{
@@ -20,8 +21,8 @@ class App extends React.Component{
         this.setState({searchfield: event.target.value})
     }
     render(){
-        const filterRobots = this.state.robots.filter(robots=>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const filterRobots = this.state.robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
      
         if(this.state.robots.length === 0){
@@ -32,7 +33,9 @@ class App extends React.Component{
         <div className='tc'>
             <h1 className='the_title'>robofriends</h1>
             <SearchBox searchChange = {this.onSearchChange} />
+            <Scroll>
             <CardList robots = {filterRobots} />
+            </Scroll>
         </div>
     )}
 }
